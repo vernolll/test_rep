2d1b41f21c65203bd8e2284150832c139aa0879b
vernolll <marina2006.karas@yandex.ru>
2024-12-11 23:18:42 +0300

    the commit where the candle files are added.* and fixed bugs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 ﻿#include <vector>
 #include <functional>
 #include <iostream>
-
-#include "tests.h"
+#include "candle.h"
 
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
